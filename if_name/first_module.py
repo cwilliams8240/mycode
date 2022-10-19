2aada712cdd2abec9810a8e0e8e49c201391613a
Mona Lisa <cwilliams8240@users.noreply.github.com>
2022-10-19 13:51:23 +0000

    Playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 3
+"""TLG Learning | CWilliams
+   if == name"""
+
+print("This code will always execute.")
+
+def main():
+
+    print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+    main()
