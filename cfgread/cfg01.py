6a959db3baae861801b19af52a141a78f28db427
Mona Lisa <cwilliams8240@users.noreply.github.com>
2022-10-21 17:07:24 +0000

    reading in data

---
+++ cfgread/cfg01.py
@@ -0,0 +1,33 @@
+#!/usr/bin/evn python3
+"""TLG Learning | CWilliams
+   Explore Read"""
+
+def main():
+
+    ## create file object in "r"ead mode
+    configfile = open("vlanconfig.cfg", "r")
+
+    ## display file to the screen - .read()
+    print(configfile.read())
+
+    ## close file
+    configfile.close()
+
+    ######## EXPLORE READLINES ########
+    ## re-create file object to explore new method
+    configfile = open("vlanconfig.cfg", "r")
+
+    ## make a list of file lines - .readlines()
+    configlist = configfile.readlines()
+    print(configlist)
+
+    ## Iterate through configlist
+    for x in configlist:
+        print(x, end="")  # by default, print() ends in a newline
+                          # by passing endd="" print will not "add a second"
+                          # newline character
+
+    ## Always close your file
+    configfile.close
+
+main()
