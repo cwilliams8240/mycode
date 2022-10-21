3413cf1152d8abc438de4cb160fcfc8e821d5655
Mona Lisa <cwilliams8240@users.noreply.github.com>
2022-10-21 13:38:17 +0000

    studying for logic

---
+++ introfor/forloop1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""TLG Learning |CWilliams
+   learning about for logic"""
+
+def main():
+    # create the list called vendors
+    vendors = ["cisco", "juniper", "big_ip", "f5", "arista"]
+    # loop across the list vendors
+    for x in vendors:
+        print("The vendor is:" + x)  # each time through the loop print value of x
+    print("\n Our loop has ended.")  # when the loop ends print this
+
+main()
+    
